6ac03b6a82f75febc9b87ea9fcbf38a5f22536f0
breno39 <breno.bpa@hotmail.com>
2021-06-20 21:45:36 -0300

    initial commit

---
+++ src/main/java/dev/tribos/wakandaacademy/WakandaacademyApplication.java
@@ -0,0 +1,13 @@
+package dev.tribos.wakandaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakandaacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tribos/wakandaacademy/WakandaacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tribos.wakandaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
