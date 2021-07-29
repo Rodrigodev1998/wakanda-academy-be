c52fc1b1fef521fd3f91ffc80f20e8d4306cdb7e
Maabia <maabia.ferry16@gmail.com>
2021-07-29 13:29:12 -0300

    jornadaClarezaService criado e iniciado

---
+++ src/main/java/dev/tribos/wakandaacademy/jornadaclareza/application/service/JornadaClarezaService.java
@@ -0,0 +1,7 @@
+package dev.tribos.wakandaacademy.jornadaclareza.application.service;
+
+import dev.tribos.wakandaacademy.jornadaclareza.domain.JornadaClareza;
+
+public interface JornadaClarezaService {
+	JornadaClareza criaJornadaClareza(JornadaClareza jornadaClareza);
+}
