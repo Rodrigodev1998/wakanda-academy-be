3b828657277278ccee7cb408916dd0982ec472c0
Maabia <maabia.ferry16@gmail.com>
2021-07-29 13:23:47 -0300

    repository jornada da clareza criado e em andamento

---
+++ src/main/java/dev/tribos/wakandaacademy/jornadaclareza/application/repository/JornadaClarezaRepository.java
@@ -0,0 +1,7 @@
+package dev.tribos.wakandaacademy.jornadaclareza.application.repository;
+
+import dev.tribos.wakandaacademy.jornadaclareza.domain.JornadaClareza;
+
+public interface JornadaClarezaRepository {
+	JornadaClareza save(JornadaClareza jornadaClareza);
+}
