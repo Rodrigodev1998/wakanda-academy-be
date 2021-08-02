26e7035bcd791bcc5762d7b4b71c3963d0c58cc7
sammolacerda <sammolacerda@gmail.com>
2021-08-02 19:18:03 -0300

    Service

--- src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/repository/JornadaDaCompetenciaRepository.java
+++ src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/repository/JornadaDaCompetenciaRepository.java
@@ -1,10 +1,12 @@
 package dev.tribos.wakandaacademy.jornadaDaCompetencia.application.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import dev.tribos.wakandaacademy.jornadaDaCompetencia.application.domain.JornadaDaCompetencia;
 import dev.tribos.wakandaacademy.wakander.domain.Wakander;
 
-public interface JornadaDaCompetenciaRepository  {
-	JornadaDaCompetencia save(JornadaDaCompetencia jornadaDaCompetencia);
+public interface JornadaDaCompetenciaRepository extends MongoRepository<JornadaDaCompetencia, String>{
+	//JornadaDaCompetencia save(JornadaDaCompetencia jornadaDaCompetencia);
 	JornadaDaCompetencia findByCodigo(String codigo);
 	Wakander buscaWakanderPorCodigo(String emailWakander);
 
--- src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/service/JornadaDaCompetenciaSpringDataMongoDBService.java
+++ src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/service/JornadaDaCompetenciaSpringDataMongoDBService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.tribos.wakandaacademy.jornadaDaCompetencia.application.domain.JornadaDaCompetencia;
@@ -16,7 +17,8 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @AllArgsConstructor
 public class JornadaDaCompetenciaSpringDataMongoDBService implements JornadaDaCompetenciaService{
-
+    
+	@Autowired
 	private JornadaDaCompetenciaRepository jornadaDaCompetenciaRepository;
 	
 	public JornadaDaCompetenciaSpringDataMongoDBService() {}
