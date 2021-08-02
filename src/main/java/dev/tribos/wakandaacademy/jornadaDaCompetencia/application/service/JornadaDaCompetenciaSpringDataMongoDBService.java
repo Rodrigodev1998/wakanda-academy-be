d2d1286e1e2bc737a36a8a496cf975b1c2ba1958
sammolacerda <sammolacerda@gmail.com>
2021-08-02 12:58:12 -0300

    construindo Service

--- src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/service/JornadaDaCompetenciaSpringDataMongoDBService.java
+++ src/main/java/dev/tribos/wakandaacademy/jornadaDaCompetencia/application/service/JornadaDaCompetenciaSpringDataMongoDBService.java
@@ -15,7 +15,7 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @Log4j2
 @AllArgsConstructor
-public class JornadaDaCompetenciaSpringDataMongoDBService implements JornadaDaCompetenciaService {
+public class JornadaDaCompetenciaSpringDataMongoDBService implements JornadaDaCompetenciaService{
 
 	private JornadaDaCompetenciaRepository jornadaDaCompetenciaRepository;
 	
