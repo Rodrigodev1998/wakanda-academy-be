cc080a312b5a0ec9acf7a4dfa5b06fedf81f8787
Vinicius Reis <vinicius.reis@msn.com>
2021-08-08 21:19:50 -0300

    organize imports

--- src/main/java/dev/tribos/wakandaacademy/handler/ErrorApiDTO.java
+++ src/main/java/dev/tribos/wakandaacademy/handler/ErrorApiDTO.java
@@ -1,12 +1,13 @@
 package dev.tribos.wakandaacademy.handler;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,9 +15,9 @@ import java.util.List;
 public class ErrorApiDTO {
 	private String message;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String description;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private List<ValidationDTO> validationsError;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private List<ValidationDTO> validationsError;
 }
