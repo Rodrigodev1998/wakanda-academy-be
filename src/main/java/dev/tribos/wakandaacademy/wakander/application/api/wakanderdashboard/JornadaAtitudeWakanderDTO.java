9bfee77ec0664cf8732ee5110dc1a8034fe67875
Vinicius Reis <vinicius.reis@msn.com>
2021-08-12 11:56:17 -0300

    implements jornada atitude no dashboard

---
+++ src/main/java/dev/tribos/wakandaacademy/wakander/application/api/wakanderdashboard/JornadaAtitudeWakanderDTO.java
@@ -0,0 +1,18 @@
+package dev.tribos.wakandaacademy.wakander.application.api.wakanderdashboard;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import dev.tribos.wakandaacademy.wakander.domain.jornadaatitude.JornadaAtitudeWakander;
+import lombok.Value;
+
+@Value
+public class JornadaAtitudeWakanderDTO {
+	List<EtapaJornadaWakanderDTO> etapas;
+	
+	public JornadaAtitudeWakanderDTO(JornadaAtitudeWakander jornadaAtitude) {
+		this.etapas = jornadaAtitude.getEtapas().stream()
+				.map(EtapaJornadaWakanderDTO::new)
+				.collect(Collectors.toList());
+	}
+}
