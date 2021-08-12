101444769cce72b79fe3fa9d433343969f3571cb
sammolacerda <sammolacerda@gmail.com>
2021-08-12 09:20:18 -0300

    adicionado @NoArgsConstructor(access = AccessLevel.PRIVATE) na JornadaDaCompetencia

--- src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaatitude/jornadaDaCompetencia/JornadaDaCompetencia.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaatitude/jornadaDaCompetencia/JornadaDaCompetencia.java
@@ -10,11 +10,13 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JornadaDaCompetencia implements EtapaJornadaAtitudeWakander {
 	@Setter
 	private String codigo;
