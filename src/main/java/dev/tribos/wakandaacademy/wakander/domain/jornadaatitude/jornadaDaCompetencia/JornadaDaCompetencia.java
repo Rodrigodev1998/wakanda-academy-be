8712d0942e9784416612300167f058b681a30b8e
felip <felipe.psic@outlook.com.br>
2021-08-11 22:28:55 -0300

    Add NoArgs em JornadaDaCompetencia

--- src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaatitude/jornadaDaCompetencia/JornadaDaCompetencia.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaatitude/jornadaDaCompetencia/JornadaDaCompetencia.java
@@ -6,15 +6,12 @@ import java.util.List;
 import dev.tribos.wakandaacademy.wakanda.domain.EtapaJornadaAtitudeWakanda;
 import dev.tribos.wakandaacademy.wakander.domain.jornadaatitude.EtapaJornadaAtitudeWakander;
 import dev.tribos.wakandaacademy.wakander.domain.jornadaatitude.StatusEtapaJornadaAtitude;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JornadaDaCompetencia implements EtapaJornadaAtitudeWakander {
 	@Setter
 	private String codigo;
