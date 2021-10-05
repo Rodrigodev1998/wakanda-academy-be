f3cb330596d90c6e17c3326afae4a2faee22d809
Vinicius Reis <vinicius.reis@msn.com>
2021-10-05 16:27:59 -0300

    implementa Jornada Conhecimento Wakander e Inicia Tribo Wakander

--- src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/JornadaConhecimentoWakander.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/JornadaConhecimentoWakander.java
@@ -1,5 +1,15 @@
 package dev.tribos.wakandaacademy.wakander.domain.jornadaconhecimento;
 
-public class JornadaConhecimentoWakander {
+import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class JornadaConhecimentoWakander {
+	private List<TriboWakander> tribos;
 }
