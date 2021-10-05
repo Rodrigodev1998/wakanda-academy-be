daea4fa2cbfa5971d4ec01bfe984524ce3ab5d13
Vinicius Reis <vinicius.reis@msn.com>
2021-10-05 16:45:45 -0300

    implementa licao wakander

--- src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/SabedoriaWakander.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/SabedoriaWakander.java
@@ -1,5 +1,25 @@
 package dev.tribos.wakandaacademy.wakander.domain.jornadaconhecimento;
 
-public class SabedoriaWakander {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SabedoriaWakander {
+	private String codigo;
+	private String nome;
+	private String descricao;
+	private String logo;
+	private LocalDateTime dataInicio;
+	private LocalDateTime dataConclusao;
+	private List<LicaoWakander> licoes;
+	private StatusJornadaConhecimento status;
 }
