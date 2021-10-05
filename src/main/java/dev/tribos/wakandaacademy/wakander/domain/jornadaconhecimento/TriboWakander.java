a370f955888a13aaff8dd703dbc6792455c34196
Vinicius Reis <vinicius.reis@msn.com>
2021-10-05 16:42:33 -0300

    implementa Tribo Wakander e Sabedoria Wakander

--- src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/TriboWakander.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/domain/jornadaconhecimento/TriboWakander.java
@@ -1,5 +1,25 @@
 package dev.tribos.wakandaacademy.wakander.domain.jornadaconhecimento;
 
-public class TriboWakander {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TriboWakander {
+	private String codigo;
+	private String nome;
+	private String descricao;
+	private String logo;
+	private LocalDateTime dataInicio;
+	private LocalDateTime dataConclusao;
+	private List<SabedoriaWakander> sabedorias;
+	private StatusJornadaConhecimento status;
 }
