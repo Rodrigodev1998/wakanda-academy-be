9b878ee4ff7ce1a6c33a95ad2e3b492975c37201
Vinicius Reis <vinicius.reis@msn.com>
2022-05-06 04:27:47 -0300

    remove required args

--- src/main/java/dev/tribos/wakandaacademy/wakander/application/api/preCadastroWakander/WakanderPreCadastroController.java
+++ src/main/java/dev/tribos/wakandaacademy/wakander/application/api/preCadastroWakander/WakanderPreCadastroController.java
@@ -8,14 +8,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import dev.tribos.wakandaacademy.wakander.application.service.WakanderService;
 import dev.tribos.wakandaacademy.wakander.domain.Wakander;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class WakanderPreCadastroController implements WakanderPreCadastroAPI {
-	private final WakanderService wakanderService;
+	private WakanderService wakanderService;
 
 	@Override
 	public ResponseEntity<WakanderPreCadastroResponse> preCadastraWakander(WakanderPreCadastroRequest wakanderPreCadastroRequest,
